0b908a2eaf5a254977969452925d2cc262e069b2
lmrocio <lmrocio@iesrafaelalberti.es>
2024-10-24 21:14:32 +0200

    Actualización

--- src/ej22_03.py
+++ src/ej22_03.py
@@ -3,30 +3,35 @@ Escribir un programa que pida al usuario un número entero positivo
 y muestre por pantalla todos los números impares desde 1 hasta ese número separados por comas
 '''
 
-# NO ESTÁ TERMINADO
+
 def datos_entrada():
     num = int(input("Introduzca un número entero positivo: "))
     return num
 
-def mostrar_impares(num):
-    cadena = ''
+def mostrar_impares(num, cadena: str):
     for i in range(2, num):
         resto = i%2
         if resto != 0:
-            candena = cadena + i
-            print(f"{i}") #ME QUEDÉ AQUI
+            cadena += str(i)
+        
         else:
-            print(", ")
-
+            cadena += ', '
+            
+    return cadena
 
 def main():
+    cadena = '1 '
     num = datos_entrada()
-    print(f"Para el número {num} encontramos los primos:\n 1")
-    mostrar_impares(num)
+    print(f"Para el número {num} encontramos los primos: ")
+    cadena = mostrar_impares(num, cadena)
     if num%2 == 0:
-        print(".")
+        cadena += '.'
+        print (cadena)
+        
     else:
-        print(f", {num}.")
+        cadena += str(num)
+        print (f"{cadena}.")
+       
 
 
 if __name__ == '__main__':
